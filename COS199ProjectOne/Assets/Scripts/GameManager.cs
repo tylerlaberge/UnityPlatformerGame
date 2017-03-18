1009c9b45d7993292a240f86c7e524fe51263936
tylerlaberge <tyler.laberge@maine.edu>
2017-03-18 16:18:11 -0400

    renamed project

---
+++ COS199ProjectOne/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	
+	public MenuManager menuManager;
+	
+	private bool running = false;
+	private int score;
+	private int levelNum = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	public void StartNewGame() 
+	{
+		Debug.Log("StartNewGame");
+		score = 0;
+		running = true;
+		LoadNextLevel();
+	}
+	
+	public void PauseGame()
+	{
+		Debug.Log("PauseGame");
+		running = false;
+	}
+	
+	public void ResumeGame()
+	{
+		Debug.Log("Resume");
+		running = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (running) {
+			menuManager.SetScore(score++);
+		}
+	}
+	
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(getNextLevelName());
+	}
+	
+	private string getNextLevelName()
+	{
+		levelNum++;
+		return "level" + levelNum;
+	}
+}
---
+++ COS199ProjectOne/Assets/Scripts/MoverX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverX : MonoBehaviour {
+	
+	public float x;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		LeanTween.moveX(this.gameObject, x, time).setEaseInOutSine().setLoopPingPong();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
